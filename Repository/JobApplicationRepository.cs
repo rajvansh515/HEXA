19b9590eaed8c71128986c4a6ac87760bfc86520
rajvansh515 <126567790+rajvansh515@users.noreply.github.com>
2024-10-05 15:45:53 +0530

    Add files via upload

---
+++ Repository/JobApplicationRepository.cs
@@ -0,0 +1,93 @@
+﻿using CareerHub.Models;
+using CareerHub.Utility;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerHub.Repository
+{
+    internal class JobApplicationRepository
+    {
+        public JobApplicationRepository()
+        {
+
+        }
+
+        public void InsertJobApplication(JobApplication jobApplication)
+        {
+            try
+            {
+                int rowsAffected = 0;
+                using (SqlConnection connection = new SqlConnection(DatabaseConnectionUtility.GetConnectionString()))
+                {
+
+                    string query = "INSERT INTO Applications (JobID, ApplicantID, ApplicationDate, CoverLetter) " +
+                                   "VALUES (@JobID, @ApplicantID, @ApplicationDate, @CoverLetter)";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+
+                        command.Parameters.AddWithValue("@JobID", jobApplication.JobID);
+                        command.Parameters.AddWithValue("@ApplicantID", jobApplication.ApplicantID);
+                        command.Parameters.AddWithValue("@ApplicationDate", jobApplication.ApplicationDate);
+                        command.Parameters.AddWithValue("@CoverLetter", jobApplication.CoverLetter);
+
+                        connection.Open();
+
+                        rowsAffected = command.ExecuteNonQuery();
+                    }
+                }
+                Console.WriteLine($"Number of rows {rowsAffected} affected");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void GetApplicationsForJob(int jobID)
+        {
+            List<JobApplication> applications = new List<JobApplication>();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(DatabaseConnectionUtility.GetConnectionString()))
+                {
+                    using (SqlCommand command = connection.CreateCommand())
+                    {
+                        command.CommandText = "SELECT * FROM Applications WHERE JobID = @JobID";
+                        command.Parameters.AddWithValue("@JobID", jobID);
+
+                        connection.Open();
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                JobApplication application = new JobApplication();
+                                application.ApplicationID = (int)reader["ApplicationID"];
+                                application.JobID = (int)reader["JobID"];
+                                application.ApplicantID = (int)reader["ApplicantID"];
+                                application.ApplicationDate = (DateTime)reader["ApplicationDate"];
+                                application.CoverLetter = (string)reader["CoverLetter"];
+                                applications.Add(application);
+                            }
+                        }
+                    }
+                }
+
+                foreach (JobApplication app in applications)
+                {
+                    Console.WriteLine(app);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Repository/JobListingRepository.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CareerHub.Models;
+using CareerHub.Utility;
+using CareerHub.Exceptions;
+
+namespace CareerHub.Repository
+{
+    internal class JobListingRepository
+    {
+        public JobListingRepository() { }
+
+        public void InsertJobListing(JobListing jobListing)
+        {
+            try
+            {
+                int rowsaffected = 0;
+                using (SqlConnection connection = new SqlConnection(DatabaseConnectionUtility.GetConnectionString()))
+                {
+                    // Define the SQL query with parameters
+                    string query = "INSERT INTO Jobs (CompanyID, JobTitle, JobDescription, JobLocation, Salary, JobType, PostedDate) " +
+                                   "VALUES (@CompanyID, @Jobtitle, @JobDescription, @JobLocation, @Salary, @JobType, @PostedDate)";
+
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+
+                        command.Parameters.AddWithValue("@CompanyID", jobListing.CompanyID);
+                        command.Parameters.AddWithValue("@Jobtitle", jobListing.JobTitle);
+                        command.Parameters.AddWithValue("@JobDescription", jobListing.JobDescription);
+                        command.Parameters.AddWithValue("@JobLocation", jobListing.JobLocation);
+                        command.Parameters.AddWithValue("@Salary", jobListing.Salary);
+                        command.Parameters.AddWithValue("@JobType", jobListing.JobType);
+                        command.Parameters.AddWithValue("@PostedDate", jobListing.PostedDate);
+
+                        connection.Open();
+
+                        rowsaffected = command.ExecuteNonQuery();
+                    }
+                }
+                Console.WriteLine($"Number of rows {rowsaffected} affected");
+            }
+            catch (Exception ex)
+            {
+                throw new DatabaseConnectionException("Database Connectivity error");
+            }
+
+        }
+
+        public void GetJobListings(List<JobListing> jobListing)
+        {
+            try
+            {
+                jobListing.Clear();
+
+                string query = "SELECT * FROM JOBS";
+
+                using (SqlConnection connection = new SqlConnection(DatabaseConnectionUtility.GetConnectionString()))
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                   
+                        connection.Open();
+
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                JobListing listing = new JobListing();
+                                listing.JobID = (int)reader["JobID"];
+                                listing.CompanyID = (int)reader["CompanyID"];
+                                listing.JobTitle = (string)reader["JobTitle"];
+                                listing.JobDescription = (string)reader["JobDescription"];
+                                listing.JobLocation = (string)reader["JobLocation"];
+                                listing.Salary = (decimal)reader["Salary"];
+                                listing.JobType = (string)reader["JobType"];
+                                listing.PostedDate = (DateTime)reader["PostedDate"];
+
+                                jobListing.Add(listing);
+                            }
+                        }
+                    }
+                }
+
+                foreach (JobListing listing in jobListing)
+                {
+                    Console.WriteLine($"Job ID: {listing.JobID}");
+                    Console.WriteLine($"Company ID: {listing.CompanyID}");
+                    Console.WriteLine($"Job Title: {listing.JobTitle}");
+                    Console.WriteLine($"Job Description: {listing.JobDescription}");
+                    Console.WriteLine($"Job Location: {listing.JobLocation}");
+                    Console.WriteLine($"Salary: {listing.Salary}");
+                    Console.WriteLine($"Job Type: {listing.JobType}");
+                    Console.WriteLine($"Posted Date: {listing.PostedDate}");
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+    }
+}
